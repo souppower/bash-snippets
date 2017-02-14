39ce972735f9e1a2f9b4bb9c54b985e8a100e85d
souppower <r.kubota92@gmail.com>
2017-02-14 15:05:10 +0900

    add prompt and pwd

---
+++ prompt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "Are you sure? (Y/n)"
+
+answer=
+while [[ ! $answer ]]; do
+    read -r -n 1 -s answer
+    if [[ $answer = [Yy] ]]; then
+        answer="yes"
+    elif [[ $answer = [Nn] ]]; then
+        answer="no"
+    fi
+done
+
+printf "\n%s\n" $answer
