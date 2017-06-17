c7a23f379881d4388c9f1fe8ea1b72e7f8fdf36f
souppower <souppower@goodpatch.com>
2017-06-17 16:26:43 +0900

    :+1: add URL prompt for website mirroring

--- mirror-website.sh
+++ mirror-website.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/bin/bash -Ceu
+
+read -r -p "URL: " url
+if [[ "$url" == "" ]]; then
+    echo "No URL was given." >&2
+    exit 1
+fi
 
 wget --mirror \
      --page-requisites \
      --quiet --show-progress \
      --no-parent \
      --convert-links \
-     --no-host-directories http://example.com
+     --no-host-directories $url
